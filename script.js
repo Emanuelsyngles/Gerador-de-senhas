62f58644ebd6dca9ce23f0ac74db804405965017
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-01-02 20:57:26 -0300

    Continuando a parte lógica.

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ let novasenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
+slider.oninput = function() {
+    sizePassword.innerHTML = this.value
+
+}
+
+function generatePassword() {
+    
+    let pass = "";
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(math.random() * n));
+        console.log(pass);
+    }
+
+}
