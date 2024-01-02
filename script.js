8b65ba7ed64615c44e2bb081ba730020a627add4
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-01-02 20:39:31 -0300

    Construindo o algoritmo

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 let sliderElement = document.querySelector("#slider");
 let  buttonElement = document.querySelector("#button");
-let
+
+
+let sizePassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+
+let containerPassword = document.querySelector("#container-password")
+
+
+let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@";
+let novasenha = "";
+
+sizePassword.innerHTML = sliderElement.value;
 
