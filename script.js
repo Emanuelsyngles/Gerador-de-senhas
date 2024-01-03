3e970f7408a13eda631f5815ee709dcbe0997ce6
Emanuel syngles <122393755+Emanuelsyngles@users.noreply.github.com>
2024-01-02 21:40:12 -0300

    Corrigindo alguns erros.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let sliderElement = document.querySelector("#slider");
-let  buttonElement = document.querySelector("#button");
+let buttonElement = document.querySelector("#button");
 
 
 let sizePassword = document.querySelector("#valor");
@@ -9,22 +9,34 @@ let containerPassword = document.querySelector("#container-password")
 
 
 let charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@";
-let novasenha = "";
+let novaSenha = "";
 
 sizePassword.innerHTML = sliderElement.value;
 
-slider.oninput = function() {
+slider.oninput = function () {
     sizePassword.innerHTML = this.value
-
 }
 
 function generatePassword() {
-    
+
     let pass = "";
 
-    for(let i = 0, n = charset.length; i < sliderElement.value; ++i) {
-        pass += charset.charAt(Math.floor(math.random() * n));
-        console.log(pass);
+    for (let i = 0, n = charset.length; i < sliderElement.value; ++i) {
+        pass += charset.charAt(Math.floor(Math.random() * n));
     }
 
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass;
+    novaSenha = pass;
+
 }
+
+function copyPassword() {
+    alert('Senha copiada com sucesso!')
+    navigator.clipboard.writeText(novaSenha);
+}
+
+
+
+
+
